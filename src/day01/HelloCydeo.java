520e0b93ef0aeea37e6f4b9ae3bec3cff35d28ea
Max09091 <max201190max@gmail.com>
2025-03-31 19:30:36 -0500

    Update HelloCydeo.java

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -4,6 +4,7 @@ public class HelloCydeo {
     public static void main(String[] args) {
        System.out.println("Hello Cydeo");
          System.out.println("Some change are adding");
+        System.out.println("One more change are adding");
 
     }
 }
